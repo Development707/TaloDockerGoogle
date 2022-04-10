aa1c431f521d84fbe0a26af76c1bac9c466cf4b3
Development <70835318+Development707@users.noreply.github.com>
2022-04-10 21:52:47 +0700

    Fix nginx

--- talo_nodejs/src/index.js
+++ talo_nodejs/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(handleError);
 // Create server
 const server = http.createServer(app);
-const io = socketio(server, { transports: ['websocket', 'jsonp-polling'] });
+const io = socketio(server);
 socketApp(io);
 routes(app, io);
 
